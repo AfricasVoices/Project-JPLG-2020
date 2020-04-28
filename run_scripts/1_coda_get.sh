bfdb2d579d7fe6178a7b8a04c4f432e69545ceab
Alexander Simpson <as2388@cam.ac.uk>
2020-04-28 14:14:44 +0100

    Add standard run scripts
    Copied from Covid19-KE-Urban, adapted for JPLG.

---
+++ run_scripts/1_coda_get.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $# -ne 3 ]]; then
+    echo "Usage: ./1_coda_get.sh <coda-auth-file> <coda-v2-root> <data-root>"
+    echo "Downloads coded messages datasets from Coda to '<data-root>/Coded Coda Files'"
+    exit
+fi
+
+AUTH=$1
+CODA_V2_ROOT=$2
+DATA_ROOT=$3
+
+./checkout_coda_v2.sh "$CODA_V2_ROOT"
+
+PROJECT_NAME="JPLG-2020"
+DATASETS=(
+    "s07e01"
+
+    "location"
+    "gender"
+    "age"
+    "recently_displaced"
+    "in_idp_camp"
+    "household_language"
+)
+
+cd "$CODA_V2_ROOT/data_tools"
+git checkout "9a9a8e708e3f20f37848a6b02f79bcee43e5be3b"  # (master which supports segmenting)
+
+mkdir -p "$DATA_ROOT/Coded Coda Files"
+
+for DATASET in ${DATASETS[@]}
+do
+    echo "Getting messages data from ${PROJECT_NAME}_${DATASET}..."
+
+    pipenv run python get.py "$AUTH" "${PROJECT_NAME}_${DATASET}" messages >"$DATA_ROOT/Coded Coda Files/$DATASET.json"
+done
---
+++ run_scripts/4_coda_add.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $# -ne 3 ]]; then
+    echo "Usage: ./4_coda_add.sh <coda-auth-file> <coda-v2-root> <data-root>"
+    echo "Uploads coded messages datasets from '<data-root>/Outputs/Coda Files' to Coda"
+    exit
+fi
+
+AUTH=$1
+CODA_V2_ROOT=$2
+DATA_ROOT=$3
+
+./checkout_coda_v2.sh "$CODA_V2_ROOT"
+
+PROJECT_NAME="JPLG-2020"
+DATASETS=(
+    "s07e01"
+
+    "location"
+    "gender"
+    "age"
+    "recently_displaced"
+    "in_idp_camp"
+    "household_language"
+)
+
+cd "$CODA_V2_ROOT/data_tools"
+git checkout "9a9a8e708e3f20f37848a6b02f79bcee43e5be3b"  # (master which supports segmenting)
+
+for DATASET in ${DATASETS[@]}
+do
+    echo "Pushing messages data to ${PROJECT_NAME}_${DATASET}..."
+
+    pipenv run python add.py "$AUTH" "${PROJECT_NAME}_${DATASET}" messages "$DATA_ROOT/Outputs/Coda Files/$DATASET.json"
+done
