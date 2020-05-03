1a66a0d62a611e790fb901ef94aa0465f5b5f3f4
Alexander Simpson <as2388@cam.ac.uk>
2020-05-03 11:29:53 +0100

    Fix run_pipeline.sh

--- run_scripts/run_pipeline.sh
+++ run_scripts/run_pipeline.sh
@@ -32,7 +32,7 @@ echo "Starting run with id '$RUN_ID'"
 ./2_fetch_raw_data.sh "$USER" "$AVF_BUCKET_CREDENTIALS_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
 
 ./3_generate_outputs.sh --profile-memory "$PERFORMANCE_LOGS_DIR/memory-$RUN_ID.profile" \
-    "$USER" "$AVF_BUCKET_CREDENTIALS_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
+    "$USER" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
 
 ./4_coda_add.sh "$CODA_PUSH_CREDENTIALS_PATH" "$CODA_TOOLS_ROOT" "$DATA_ROOT"
 
