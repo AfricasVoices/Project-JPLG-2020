0e07c6903014ef87cfce0a3e59d4cb954c7935c7
Alexander Simpson <as2388@cam.ac.uk>
2020-05-20 10:47:01 +0100

    Upload Mogadishu maps to Drive

--- automated_analysis.py
+++ automated_analysis.py
@@ -712,6 +712,15 @@ if __name__ == "__main__":
                 path, f"{pipeline_configuration.drive_upload.analysis_graphs_dir}/maps/districts/",
                 target_folder_is_shared_with_me=True
             )
+
+        log.info("Uploading Mogadishu maps to Drive...")
+        paths_to_upload = glob(f"{output_dir}/maps/mogadishu/*.png")
+        for i, path in enumerate(paths_to_upload):
+            log.info(f"Uploading map {i + 1}/{len(paths_to_upload)}: {path}...")
+            drive_client_wrapper.update_or_create(
+                path, f"{pipeline_configuration.drive_upload.analysis_graphs_dir}/maps/mogadishu/",
+                target_folder_is_shared_with_me=True
+            )
     else:
         log.info("Skipping uploading to Google Drive (because the pipeline configuration json does not contain the key "
                  "'DriveUploadPaths')")
